c81a13b298bd541a6c8c255857384fd2cd9126e1
Usama Nadeem <usamanadeemparacha@gmail.com>
2025-03-17 13:19:56 -0500

    Removed ignored files from tracking

--- dist/constants/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.API_ENDPOINTS = exports.ERROR_MESSAGES = void 0;
-exports.ERROR_MESSAGES = {
-    STORE_ID_REQUIRED: "store_id is required",
-};
-exports.API_ENDPOINTS = {
-    PRINTFUL_BASE_URL: "https://api.printful.com",
-};
--- dist/routes/printful-stores.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const stores_controller_1 = require("../controllers/stores.controller");
-const auth_1 = require("../middlewares/auth");
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-// Get store
-router.get("/", auth_1.authenticate, stores_controller_1.getStore);
-exports.default = router;
